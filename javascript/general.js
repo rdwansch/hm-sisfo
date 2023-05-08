09cdbd6a43728e02b0a1ab80c1229f84299aa43a
Maulana Yusuf <maulanayuusuf023@gmail.com>
2023-05-08 10:41:02 +0700

    repair : navbar component, search page and detail discussion page

--- javascript/general.js
+++ javascript/general.js
@@ -1,8 +1,13 @@
 const mulaiPencarian = document.getElementById('mulaiPencarian');
 const searchInput = document.querySelector('.search__input');
+// --
+const navMenus = document.querySelectorAll('.nav__menu');
+// --
+const starClicked = document.querySelectorAll('.star-clicked');
+const starUnclicked = document.querySelectorAll('.star-unclicked');
 
 
-mulaiPencarian.addEventListener('click', () => {
+mulaiPencarian?.addEventListener('click', () => {
     searchInput.style.width = '80%';
     searchInput.classList.add('search__input--active');
     searchInput.classList.add('rounded-start-1');
@@ -13,3 +18,92 @@ mulaiPencarian.addEventListener('click', () => {
     mulaiPencarian.classList.remove('rounded-0');
     mulaiPencarian.classList.add('rounded-end-1');
 })
+
+navMenus?.forEach(navMenu => {
+    navMenu.addEventListener('click', el => {
+        const keEldariEl = []
+        const valNav = {
+            'penelitian': 0,
+            'diskusi': 1,
+            'anggota': 2,
+        }
+
+        navMenus.forEach(navMenu => {
+            if (navMenu == el.target) {
+                keEldariEl.push(valNav[el.target.getAttribute('data-bs-nav')])
+            }
+
+            if (navMenu.classList.contains('nav-active') || navMenu.classList.contains('nav-active-once')) {
+                keEldariEl.push(valNav[navMenu.getAttribute('data-bs-nav')])
+
+                navMenu.classList.remove('nav-active')
+                navMenu.classList.remove('nav-active-once')
+
+                const classForNav = keEldariEl[0] < keEldariEl[1] ? 'from-left' : 'from-right'
+
+                navMenu.classList.add(classForNav, "change-active")
+                setTimeout(() => {
+                    navMenu.classList.remove(classForNav, "change-active")
+                }, 250)
+            }
+
+            if (navMenu == el.target) {
+                const classForNav = keEldariEl[0] < keEldariEl[1] ? 'from-left' : 'from-right'
+                setTimeout(() => {
+                    el.target.classList.add(classForNav, "change-active")
+                }, 250)
+
+                setTimeout(() => {
+                    navMenu.classList.remove(classForNav)
+                    el.target.classList.add('nav-active')
+                }, 500)
+
+                const dataNav = el.target.getAttribute('data-bs-nav')
+
+                const dataNavTarget = document.querySelector(`.${dataNav}-wrapper`)
+                dataNavTarget.classList.remove('d-none')
+                const menuContentWrapper = document.querySelector('.menu-content-wrapper')
+
+                for (child of menuContentWrapper.children) {
+                    if (child != dataNavTarget) {
+                        child.classList.add('d-none')
+                    }
+                }
+            }
+        })
+    })
+})
+
+starUnclicked?.forEach(star => {
+    star.addEventListener('click', () => {
+        star.classList.add('d-none');
+        // star clicked active
+        star.parentNode.children[1].classList.remove('d-none');
+    })
+})
+
+starClicked?.forEach(star => {
+    star.addEventListener('click', () => {
+        star.classList.add('d-none');
+        // star unclicked active
+        star.parentNode.children[0].classList.remove('d-none');
+    })
+})
+
+// code for initiate ckeditor
+ClassicEditor?.create(document.querySelector('#editor'))
+    .then(editor => {
+        const ckToolbar = document.querySelector('.ck.ck-toolbar__items');
+
+        ckToolbar.childNodes[11].classList.add('d-none');
+        ckToolbar.childNodes[12].classList.add('d-none');
+        // console.log(ckToolbar.childNodes);
+
+        editor.editing.view.change(writer => {
+            writer.setStyle('min-height', '123px', editor.editing.view.document.getRoot());
+        });
+        window.editor = editor;
+    })
+    .catch(error => {
+        console.error(error);
+    });
